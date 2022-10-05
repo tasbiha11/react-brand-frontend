6092630683e8ceff373ddd6b23e2d04d5c27401a
tasbiha11 <tasbihahussain@yahoo.com>
2022-10-06 01:39:14 +0600

    hero-section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import bgImg from '../assets/cyber-bg.png'
+
+const Hero = () => {
+    return (
+        <div name='home' className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+            <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+                <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+                    <p className='text-2xl'>Unique Sequencing & Production</p>
+                    <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
+                    <p className='text-2xl'>This is our Tech brand.</p>
+                    <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
+                </div>
+                <div>
+                    <img className='w-full' src={bgImg} alt="/" />
+                </div>
+            </div>
+            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[-9%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
+            border border-slate-300 rounded-xl text-center shadow-xl'>
+                <p>Data Services</p>
+                <div className='flex justify-between flex-wrap px-4'>
+                    <p className='flex px-4 py-2 text-slate-500'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 text-indigo-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                    </svg>
+                        App Security</p>
+                    <p className='flex px-4 py-2 text-slate-500'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 text-indigo-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                    </svg>
+                        Dashboard Design</p>
+                    <p className='flex px-4 py-2 text-slate-500'> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 text-indigo-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                    </svg>
+                        Cloud Data</p>
+                    <p className='flex px-4 py-2 text-slate-500'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 text-indigo-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                    </svg>API</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Hero
