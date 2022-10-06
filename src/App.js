2ab8430ce7584a4c2c4f0be996c7c6cd11e072f5
tasbiha11 <tasbihahussain@yahoo.com>
2022-10-07 01:50:25 +0600

    subscription

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Support from "./components/Support.jsx";
 import About from "./components/About";
+import Subscription from "./components/Subscription";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Support />
       <AllinOne />
+      <Subscription />
     </>
   );
 }
